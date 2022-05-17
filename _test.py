82109fa151e83b9d5e915d9b86f32b46a342f0f0
axel.auza <axel.auza@funiber.org>
2022-05-17 11:13:20 -0500

    Adding requeriments for testing and sample file salary

---
+++ _test.py
@@ -0,0 +1,18 @@
+from company import Company
+from file import File
+from parser import Parser
+from person import Person
+
+def test_payEmployeeRENE():
+    test={'RENE': {'MO': '10:00-12:00', 'TU': '10:00-12:00', 'TH': '01:00-03:00', 'SA': '14:00-18:00', 'SU': '20:00-21:00'}}
+    person=Person(list(test.keys())[0])
+    company=Company()    
+    assert company.payEmployee(person,test[person.getName()]) == 215
+
+def test_payEmployeeASTRID():
+    test={'ASTRID': {'MO': '10:00-12:00', 'TH': '12:00-14:00', 'SU': '20:00-21:00'}}
+    person=Person(list(test.keys())[0])
+    company=Company()    
+    assert company.payEmployee(person,test[person.getName()]) == 85
+
+
